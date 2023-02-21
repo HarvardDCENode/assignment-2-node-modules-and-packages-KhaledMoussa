408db50f18eb87288fb5b5bfa50a9cbab3ca32f4
KhaledMoussa <91042480+KhaledMoussa@users.noreply.github.com>
2023-02-20 22:29:26 -0500

    Update simple-http-file-server.js

--- simple-http-file-server.js
+++ simple-http-file-server.js
@@ -2,7 +2,7 @@
 // access the app from you browser at http://localhost:8080/ (add any arbitrary path)
 
 const http = require('http');
-//const url = require('url');
+const url = require('url');
 const path = require('path');
 const fs = require('fs');
 

fa4815b7e948f14f07dc0288b8c352965f6faec7
KhaledMoussa <91042480+KhaledMoussa@users.noreply.github.com>
2023-02-20 22:24:20 -0500

    Add files via upload

---
+++ mime.js
@@ -0,0 +1,20 @@
+var mime = function(){
+
+  return
+  {
+    log : function(extname){
+    const mimeTypes = {
+      'html' : "text/html",
+      'css'  : "text/css",
+      'js'   : "text/javascript",
+      'png'  : "image/png",
+      'jpg'  : "image/jpg"
+    }
+
+    console.log(mimeTypes[extname]);
+
+                    }
+}
+}
+
+modules.exports = mime;
